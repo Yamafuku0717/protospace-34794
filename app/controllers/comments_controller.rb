53661bf5d1fa3346c59c260abb490a01428d8868
Yamafuku0717 <toyaman8339@yahoo.co.jp>
2021-03-12 19:54:19 +0900

    render部分修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render prototype_path(@comment.prototype)
+      render "prototypes/show"
     end
   end
 
